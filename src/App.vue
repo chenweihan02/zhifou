bc7f039addd95f7a9722e12137498b88b1ecf4d2
chenweihan <1034558980@qq.com>
2022-11-12 09:47:07 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
+import HelloWorld from './components/HelloWorld.vue'
+import ColumnList from './components/ColumnList.vue'
+import GlobalHeader from './components/GlobalHeader.vue'
+import DropDown from './components/DropDown.vue'
+
+
+</script>
+
+<template>
+    <global-header />
+    <drop-down />
+
+
+    <column-list />
+  
+  <!-- <div>
+    <a href="https://vitejs.dev" target="_blank">
+      <img src="/vite.svg" class="logo" alt="Vite logo" />
+    </a>
+    <a href="https://vuejs.org/" target="_blank">
+      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+  </div>
+  <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="bg-blue-600 h-14">
+    <div class="flex max-w-screen-xl mx-auto justify-between h-full items-center">
+      <h1 class="text-xl text-white">知否知否</h1>
+      <!-- v-if="!user.isLogin" -->
+      <div v-if="!isLogin">
+        <a class="btn" href="#">登录</a>
+        <a class="btn" href="#">注册</a>
+      </div>
+      <div v-else>
+        <a class="btn" href="#">你好 xxx</a>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+ import { defineComponent, PropType, ref } from 'vue';
+
+//  interface UserProps {
+//     isLogin: boolean
+//     name?: string
+//     id?: number
+//   }
+
+  const isLogin = ref(true)
+
+  // defineProps ({
+  //     user: {
+  //       type: Object as PropType<UserProps>,
+  //       required: true
+  //    }
+  // })
+
+//   export interface UserProps {
+//     isLogin: boolean
+//     name?: string
+//     id?: number
+//   }
+
+//  export default defineComponent({
+//    name: 'GlobalHeader',
+//    props: {
+//      user: {
+//       type: Object as PropType<UserProps>,
+//       required: true
+//      }
+//    }
+//  })
+
+
+</script>
+
+
+<style scoped>
+  .btn {
+    @apply border px-4 py-2 text-white rounded mx-1 hover:text-black hover:bg-white
+  }
+
+</style>
